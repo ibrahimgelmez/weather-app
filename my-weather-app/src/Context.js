d5e46989d884bec22c9fdbbaec3aee9bc72ec26d
Ibrahim Gelmez <ibrg47@gmail.com>
2023-03-07 10:50:13 +0300

    i fixed it some spaghetti codes

--- my-weather-app/src/Context.js
+++ my-weather-app/src/Context.js
@@ -8,6 +8,8 @@ export default function ContextProvider({children}){
   const [weatherData,setWeatherData] = useState("")
   const [ready,setReady] = useState("off")
 
+  const [iconUrl,setIconUrl] =useState("")
+
   //Geocoder API
   useEffect(()=>{
     fetch( `https://api.openweathermap.org/geo/1.0/direct?q=${city.cityName}&limit=1&appid=055820ae072cf82226acf17703157acb`)
@@ -25,8 +27,12 @@ export default function ContextProvider({children}){
   useEffect(()=>{    
     fetch( `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=24e136864d3968c091ce8a05ed604d09` )
     .then(res => res.json())
-    .then(data => setWeatherData(data))  
+    .then(data => {
+      setIconUrl(data.weather[0].icon)
+      setWeatherData(data)
+    })  
   },[location.name])
+  
  
    
 
@@ -37,7 +43,7 @@ export default function ContextProvider({children}){
 
 
   return(
-    <Context.Provider value={{city,setCity,location,weatherData,setReady,ready,toggleReady}}>
+    <Context.Provider value={{city,setCity,location,weatherData,setReady,ready,toggleReady,iconUrl}}>
       {children}
     </Context.Provider>
   )
--- my-weather-app/src/components/WeatherDisplayCart.js
+++ my-weather-app/src/components/WeatherDisplayCart.js
@@ -2,8 +2,7 @@ import React,{useContext,useState} from "react"
 import { Context } from "../Context"
 
 export default function WeatherDisplayCart(){
-  const {city,weatherData,location} = useContext(Context)
-  console.log(weatherData)
+  const {city,weatherData,location,iconUrl} = useContext(Context)
 
   const [hour,setHour] = useState(new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}))
   const condition = weatherData && weatherData.cod !== '400' ? <h1>{(weatherData.main.temp - 273.15).toFixed(2)} °C</h1>:<h2>Hello</h2> 
@@ -13,7 +12,7 @@ export default function WeatherDisplayCart(){
           { location.name &&  <h2>{location.name}</h2>}
           { location.name && <p>{hour}</p>}
           { location.name && condition}
-          {location.name && <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}></img>}
+          {location.name && <img src={`http://openweathermap.org/img/wn/${iconUrl}.png`}></img>}
       </div>
     </>
   )
