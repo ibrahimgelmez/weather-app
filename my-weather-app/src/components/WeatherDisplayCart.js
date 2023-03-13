514abb47bc3e8928f341d0ace76a38cf07cf096a
Ibrahim Gelmez <ibrg47@gmail.com>
2023-03-13 14:05:35 +0300

    ui improvements

--- my-weather-app/src/components/WeatherDisplayCart.js
+++ my-weather-app/src/components/WeatherDisplayCart.js
@@ -5,17 +5,17 @@ function WeatherDisplayCart(){
   const {city,weatherData,location,iconUrl} = useContext(Context)
 
   const [hour,setHour] = useState(new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}))
-  const condition = weatherData && weatherData.cod !== '400' ? <h1 className="weather--temp">{(weatherData.main.temp - 273.15).toFixed(2)} °C</h1>:<h2>Hello</h2> 
+  const condition = weatherData && weatherData.cod !== '400' ? <h1 className="weather--temp text-5xl font-bold">{(weatherData.main.temp - 273.15).toFixed(2)} °C</h1>:<h2>Hello</h2> 
   return(
     <>
-      {location.name && <div className="display-cart">
+      {location.name && <div className="display-cart w-[620px] h-[300px] mr-4 ring-4 ring-gray-800 shadow-2xl ">
         <div className="city-time-data">
-          { location.name &&  <p className="city">{location.name}</p>}
-          { location.name && <p className="time">{hour}</p>}
+          { location.name &&  <p className="city text-white font-bold ">{location.name}</p>}
+          { location.name && <p className="time text-white shadow-lg bg-gray-900 rounded-lg">{hour}</p>}
         </div>
         <div className="temp-icon-data">
           { location.name && condition}
-          {location.name && <img className="weather--icon" src={`http://openweathermap.org/img/wn/${iconUrl}@2x.png`}></img>}
+          {location.name && <img className="weather--icon w-32 mt-2" src={`http://openweathermap.org/img/wn/${iconUrl}@2x.png`}></img>}
         </div>
       </div>}
     </>
--- my-weather-app/src/components/WeatherScreen.js
+++ my-weather-app/src/components/WeatherScreen.js
@@ -14,21 +14,23 @@ import { Context } from "../Context";
     })
   }
   return(
-    <>
-      <h1>Weather App</h1>
+    <div className="text-center">
+      <h1 className="text-5xl font-bold m-9 text-white ">Weather App</h1>
 
       <form>
         <input 
+        className="mt-8 mb-12 w-[460px]  pl-3 py-2 rounded ring-2 ring-black focus:outline-none font-bold text-gray-900" 
         placeholder="City Name" 
         name="cityName" 
         value={city.cityName} 
         onChange={handleChange}
         ></input>
-        <button type="button" onClick={toggleReady}>Submit</button>
+        <button className="bg-sky-500 py-2 px-8 rounded ring-1 ml-3 text-md text-white font-bold hover:ring-2
+        hover:ring-sky-600 active:translate-y-1 transform transition" type="button" onClick={toggleReady}>Submit</button>
       </form>
       
 
-    </>
+    </div>
     
   )
 }
