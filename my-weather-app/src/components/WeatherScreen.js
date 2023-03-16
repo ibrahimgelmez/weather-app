19121d5a0f6800839ba1ab6a32946cf2949d0ec5
Ibrahim Gelmez <ibrg47@gmail.com>
2023-03-16 15:39:17 +0300

    Update WeatherScreen.js

--- my-weather-app/src/components/WeatherScreen.js
+++ my-weather-app/src/components/WeatherScreen.js
@@ -24,6 +24,7 @@ import { Context } from "../Context";
         name="cityName" 
         value={city.cityName} 
         onChange={handleChange}
+        
         ></input>
         <button className="bg-sky-500 py-2 px-8 rounded ring-1 ml-3 text-md text-white font-bold hover:ring-2
         hover:ring-sky-600 active:translate-y-1 transform transition" type="button" onClick={toggleReady}>Submit</button>
